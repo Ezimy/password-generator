b31cfa568cc54c37243266bc49b0978abcf4855d
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-09-22 19:16:06 -0700

    javascript

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-
-
+let password = "";
+let passEl1 = document.getElementById("pass-el-1")
+let passEl2 = document.getElementById("pass-el-2")
+function generateRandomIndex(){
+        return Math.floor(Math.random() * characters.length)
+    }
+function generatePassword(){
+    for (let i = 0 ; i < 15; i++){
+        password += characters[generateRandomIndex()]
+    }
+    return password
+}
+function clear(){
+    password = ""
+    passEl1.textContent = ""
+    passEl2.textContent = ""
+}
+function buttonPressed(){
+    clear()
+    passEl1.textContent = generatePassword()
+    passEl2.textContent = generatePassword()
+}
 
 
