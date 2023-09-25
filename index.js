ce5c95e78d7216b547677e7267a33dfc23bafc23
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-09-25 12:50:44 -0700

    toggle symbol function is added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let characters =[];
+const lettersandnumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let checkSymbols = false;
 let password = "";
 let passEl1 = document.getElementById("pass-el-1")
 let passEl2 = document.getElementById("pass-el-2")
@@ -8,16 +10,26 @@ let input = document.getElementById("pass-length")
 function setPassLength(){
     passLength = input.value
 }
+function toggleSymbols() {
+    // Update the checkSymbols variable based on the checkbox state
+    checkSymbols = document.getElementById("toggle-el").checked;
+}
 function generateRandomIndex(){
         return Math.floor(Math.random() * characters.length)
     }
-function generatePassword(){
-    password = "";
-    for (let i = 0 ; i < passLength; i++){
-        password += characters[generateRandomIndex()]
+    function generatePassword() {
+        password = "";
+        characters = [];
+        if (checkSymbols) {
+            characters = characters.concat(lettersandnumbers, symbols);
+        } else {
+            characters = characters.concat(lettersandnumbers);
+        }
+        for (let i = 0; i < passLength; i++) {
+            password += characters[generateRandomIndex()];
+        }
+        return password;
     }
-    return password
-}
 function clear(){
     password = ""
     passEl1.textContent = ""
